64ae6e78a9149af0e6df621059fb48e2685fed30
era-trin <ellan.amarasinghe26@trinityschoolnyc.org>
2025-08-10 13:21:51 -0400

    test

--- server.py
+++ server.py
@@ -5,7 +5,9 @@ import json
 
 app = Flask(__name__, static_url_path='',static_folder = 'static')
 
-
+@app.route('/')
+def home_page():
+    return render_template("index.html", page='home')
 
 if __name__ == '__main__': 
     app.run(debug=True, port = 8000)
