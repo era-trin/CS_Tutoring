a2330a92312271ab40d7c6991e19f42a382727c7
era-trin <ellan.amarasinghe26@trinityschoolnyc.org>
2025-08-10 13:19:30 -0400

    first commit

---
+++ server.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request, jsonify
+import pandas as pd
+import json 
+
+
+app = Flask(__name__, static_url_path='',static_folder = 'static')
+
+
+
+if __name__ == '__main__': 
+    app.run(debug=True, port = 8000)
