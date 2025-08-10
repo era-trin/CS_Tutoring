15798e76e05e3c2089be2de071d341b084708589
era-trin <ellan.amarasinghe26@trinityschoolnyc.org>
2025-08-10 14:55:19 -0400

    updates
    Co-authored-by: jmpredd1e <jmpredd1e@users.noreply.github.com>

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
+
 from flask import Flask, render_template, request, jsonify
 import pandas as pd
 import json 
+
 
 
 app = Flask(__name__, static_url_path='',static_folder = 'static')
@@ -8,6 +10,14 @@ app = Flask(__name__, static_url_path='',static_folder = 'static')
 @app.route('/')
 def home():
     return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
 
 if __name__ == '__main__': 
     app.run(debug=True, port = 8000)
